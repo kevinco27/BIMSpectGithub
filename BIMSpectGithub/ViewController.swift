667af62a3fb420e6b97cb668cf33d8a6ca51b134
kevinco27 <kevinco27@hotmail.com>
2015-11-17 15:02:19 +0800

    added iOS project to github repo

---
+++ BIMSpectGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BIMSpectGithub
+//
+//  Created by kai on 2015/11/17.
+//  Copyright © 2015年 kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
